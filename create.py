60f58fb20095f3395f22fbb797450e2da96760a5
mark.kharakh <mkharakh2@gmail.com>
2016-01-05 11:47:35 -0500

    added to item table

--- create.py
+++ create.py
@@ -7,13 +7,13 @@ c = conn.cursor()
 q = "create table users(username text, password text, id integer)"
 c.execute(q)
 
-q = "create table posts(name text, grade integer, id integer)"
+q = "create table posts(name text, id integer)"
 c.execute(q)
 
 #q = "create table messages(name text, grade integer, id integer)"
 #c.execute(q)
 
-q = "create table items(name text, price integer, id integer)"
+q = "create table items(name text, price real, condition integer, category text, id integer)"
 c.execute(q)
 
 conn.commit()
