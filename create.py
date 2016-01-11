aa7d15016cd01a6073a168bd97e4943ae1a1abf9
mark.kharakh <mkharakh2@gmail.com>
2016-01-11 11:33:27 -0500

    added ids

--- create.py
+++ create.py
@@ -4,16 +4,16 @@ conn = sqlite3.connect("bs.db")
 
 c = conn.cursor()
 
-q = "create table users(username text, password text, id integer)"
+q = "create table users(username text, password text, UserId integer)"
 c.execute(q)
 
-q = "create table posts(content text, id integer)"
+q = "create table posts(content text, title text, BlogId integer, UserID integer)"
 c.execute(q)
 
 #q = "create table messages(message text, grade integer, id integer)"
 #c.execute(q)
 
-q = "create table items(name text, price real, condition integer, category text, description text, id integer)"
+q = "create table items(name text, price real, condition integer, category text, description text, ItemId integer, UserID integer)"
 c.execute(q)
 
 conn.commit()
