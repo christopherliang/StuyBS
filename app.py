0ce44aa9178339f6608cf1702e42a28d259d07f4
Chris Liang <chrisliang98@gmail.com>
2016-01-14 11:21:53 -0500

    LOGIN WORKS OWOWOWOWWOW

--- app.py
+++ app.py
@@ -1,65 +1,53 @@
 import urllib2,json
 from flask import Flask, render_template, redirect, url_for, request, Response, session
+import utils
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def landing():
-    if 'logged_in' not in session:
-        session['logged_in'] = False
-    if 'user' not in session:
-        session['user'] = 'Anonymous'
-    if request.method=="GET":
+    if request.method== "GET":
+        if 'logged_in' not in session:
+            session['logged_in'] = False
         if 'logged_in':
             return render_template("landing.html")
-        
         else:        
             return render_template("home.html", user=session['user'])
-
-@app.route("/login",methods=["GET","POST"])
-def login():
-    if request.method=="GET":
-        return render_template("landing.html")
-    if request.method=="POST":
+    if request.method == "POST":
         button = request.form['button']
-        username=request.form['username']
-        password=request.form['password']
-        print button
-        print username
-        print password
-        if button=="Login":
+        #Login procedure
+        if button == "Login":
+            username=request.form['username']
+            password=request.form['password']
             result = utils.authenticate(username,password)
-            if result == "success":
+            if result:
                 currentUser = username
                 session['user'] = username
                 session['logged_in'] = True
                 return render_template("home.html",user=username)
             else:
             	return render_template("landing.html", loginerror = "Username and password do not match!")
-        else:
-            return "bye"
-            
-@app.route("/register", methods = ["POST"])
-def register():
-    if request.method=="POST":
-        r_username=request.form['r_username']
-        r_password=request.form['r_password']
-        r_password2=request.form['r_password2']
-        if button=="Register":
-            if r_password == r_password2:
-                response = utils.add(r_username,r_password)
-                print response
-                if response == "taken":
-                    return render_template("register.html", taken = True, success = False, s=session)
-                elif response == "success":
-                    return render_template("register.html", taken = False, success = True, s=session)
+        #Register procedure
+        if button == "Register":
+            username=request.form['r_username']
+            password1=request.form['r_password1']
+            password2=request.form['r_password2']
+            #password match check
+            if (password1 == password2):
+                #username and password lengths check
+                if len(username)<4:
+                    return render_template('landing.html',registererror="Username must be longer than 4 characters")
+                if len(password1)<4:
+                    return render_template('landing.html',registererror="Password must be longer than 4 characters")
+                if utils.newUser(username,password1):
+                    return render_template("home.html");
                 else:
-                    return "Wrong combo"
+                    return render_template("landing.html",registererror="Username already taken");
             else:
-                print "Passwords don't match"
+                return render_template("landing.html",registererror="Passwords do not match!");
         else:
-            return "bye"
-        
+            render_template("landing.html");
+
 if __name__ == "__main__":
     app.debug = True    
     app.secret_key="stuylegacy"
