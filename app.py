94695ebf0e55713642f9a8f8a6632325099ba4dc
Chris Liang <chrisliang98@gmail.com>
2016-01-21 11:44:17 -0500

    polished login

--- app.py
+++ app.py
@@ -5,28 +5,23 @@ import utils
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
-def landing():
+@app.route("/home", methods=["GET","POST"])
+@app.route("/home/", methods=["GET","POST"])
+def home():
     if request.method== "GET":
-        if 'logged_in' not in session:
-            session['logged_in'] = False
-        if 'logged_in':
-            return render_template("landing.html")
-        else:        
-            return render_template("home.html", user=session['user'])
+        return render_template("home.html")
     if request.method == "POST":
         button = request.form['button']
         #Login procedure
         if button == "Login":
             username=request.form['username']
             password=request.form['password']
-            result = utils.authenticate(username,password)
-            if result:
-                currentUser = username
-                session['user'] = username
-                session['logged_in'] = True
-                return render_template("home.html",user=username)
+            if utils.authenticate(username,password):
+                if 'n' not in session:
+                    session['n'] = username
+                    return render_template("home.html")
             else:
-            	return render_template("landing.html", loginerror = "Username and password do not match!")
+                return render_template("home.html",loginerror="Invalid Username or Password")
         #Register procedure
         if button == "Register":
             username=request.form['r_username']
@@ -36,17 +31,23 @@ def landing():
             if (password1 == password2):
                 #username and password lengths check
                 if len(username)<4:
-                    return render_template('landing.html',registererror="Username must be longer than 4 characters")
+                    return render_template('home.html',registererror="Username must be longer than 4 characters")
                 if len(password1)<4:
-                    return render_template('landing.html',registererror="Password must be longer than 4 characters")
+                    return render_template('home.html',registererror="Password must be longer than 4 characters")
                 if utils.newUser(username,password1):
-                    return render_template("home.html");
+                    return render_template("home.html",registererror="Account created successfully")
                 else:
-                    return render_template("landing.html",registererror="Username already taken");
+                    return render_template("home.html",registererror="Username already taken")
             else:
-                return render_template("landing.html",registererror="Passwords do not match!");
+                return render_template("home.html",registererror="Passwords do not match!")
         else:
-            render_template("landing.html");
+            return render_template("home.html")
+
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    #remove the username from the session if it's there
+    session.pop('n', None)
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.debug = True    
